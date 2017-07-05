9317017d7da80009f07201764e3ce04af5911fb5
thanhledev <thanhledev@users.noreply.github.com>
2017-07-06 02:43:47 +0700

    Test commit code version 3

---
+++ addtwo.js
@@ -0,0 +1,8 @@
+/**
+ * Created by michael on 7/6/2017.
+ */
+var base = 2;
+
+exports.addtwo = function (input) {
+    return parseInt(input) + base;
+}
--- main.js
+++ main.js
@@ -2,29 +2,11 @@
  * Created by michael on 7/6/2017.
  */
 var http = require('http');
-var fs = require('fs');
+var addtwo = require('./addtwo.js').addtwo;
 
 http.createServer(function (req,res) {
-    var name = require('url').parse(req.url, true).query.name;
-    if (name === undefined) name = ' world';
-    if (name === 'burningbird') {
-        var file = 'phoenix5a.png';
-        fs.stat(file, function (err, stat) {
-            if(err) {
-                console.log(err);
-                res.writeHead(200, {'Content-Type':'text-plain'});
-                res.end("Sorry, Burningbird isn't around right now. \n");
-            } else {
-                var img = fs.readFileSync(file);
-                res.contentType = 'image/png';
-                res.contentLength = stat.size;
-                res.end(img, 'binary');
-            }
-        })
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello ' + name + '\n');
-    }
+    var base = 10;
+    console.log(addtwo(base));
 }).listen(8124);
 
 console.log('Server running at port 8124/');
